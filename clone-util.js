1533a26b6a2ee10a66121259c48d5982af76bce9
Scott M. Cullum <scott.cullum@akqa.com>
2015-05-16 12:45:27 -0400

    formatting cleanup

--- clone-util.js
+++ clone-util.js
@@ -12,23 +12,22 @@ var notice = clc.blue;
 
 // remove directory;
 rimraf(targetDir, function(err) {
-    if (err) console.log(error(err));
+  if (err) console.log(error(err));
 });
 
 // clone all users repo
 github.request({
-    path: "/users/scullum/repos"
+  path: "/users/scullum/repos"
 }, function(err, repos) {
-    if (err) console.log(error(err));
-    _.each(repos, function(data) {
-    	var name = data.name;
-        clone(data.clone_url, targetDir + name, {}, function(err) {
-            if (err) {
-                console.log(error(err));
-                // return;
-            }
-            console.log(notice(name) + ' clone has completed');
-            console.log('****************');
-        });
+  if (err) console.log(error(err));
+  _.each(repos, function(data) {
+    var name = data.name;
+    clone(data.clone_url, targetDir + name, {}, function(err) {
+      if (err) {
+        console.log(error(err));
+        return;
+      }
+      console.log(notice(name) + ' clone has completed \n ');
     });
+  });
 });
