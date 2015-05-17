119145ba4b02dfade4bd4b1e50039b4d7a22acad
Scott M. Cullum <scott.cullum@akqa.com>
2015-05-17 10:40:15 -0400

    adding user flag

--- clone-util.js
+++ clone-util.js
@@ -3,21 +3,34 @@ var github = require('github-request');
 var clone = require('git-clone');
 var rimraf = require('rimraf');
 var chalk = require('chalk');
+var opts = require('nomnom').parse();
 
 
 //output colors;
 var error = chalk.bgRed;
 var notice = chalk.bgBlue;
 
+var targetDir = './output/';
+var user = opts.user;
+
+if(!user) {
+  console.log(error('No user specified'));
+  return;
+}
+
+
 // remove directory;
 rimraf(targetDir, function(err) {
-  if (err) console.log(error(err));
-  else console.log(notice('Removing old clones!')+"\n");
+  if (err) {
+    console.log(error(err));
+    return;
+  }
+  else console.log(notice('Removing old clones!')+'\n');
 });
 
 // clone all users repo
 github.request({
-  path: "/users/scullum/repos"
+  path: '/users/' + user + '/repos'
 }, function(err, repos) {
   if (err) console.log(error(err));
   _.each(repos, function(data) {
